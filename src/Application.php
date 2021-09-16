f96aebd2edcc3461c36089f9fd7ea11364dc4fd7
lyl <254364502@qq.com>
2021-09-16 11:19:28 +0800

    去除exit函数

--- src/Application.php
+++ src/Application.php
@@ -2,6 +2,7 @@
 namespace yii2swoole\webserver;
 
 use Swoole\Http\Server;
+use yii\base\ExitException;
 
 /**
  * Class Application
@@ -11,4 +12,24 @@ use Swoole\Http\Server;
 class Application extends \yii\web\Application
 {
     public $swooleServer;
+
+    public function end($status = 0, $response = null)
+    {
+        if ($this->state === self::STATE_BEFORE_REQUEST || $this->state === self::STATE_HANDLING_REQUEST) {
+            $this->state = self::STATE_AFTER_REQUEST;
+            $this->trigger(self::EVENT_AFTER_REQUEST);
+        }
+
+        if ($this->state !== self::STATE_SENDING_RESPONSE && $this->state !== self::STATE_END) {
+            $this->state = self::STATE_END;
+            $response = $response ?: $this->getResponse();
+            $response->send();
+        }
+
+        if (YII_ENV_TEST) {
+            throw new ExitException($status);
+        } else {
+            return $status;
+        }
+    }
 }
